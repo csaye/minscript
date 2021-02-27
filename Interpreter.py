4c8ad9ef26a1023cde139231cf9dd0addcc9abb9
Cooper Saye <coopersaye@gmail.com>
2021-02-27 16:41:05 -0700

    Implement skip command.

--- Interpreter.py
+++ Interpreter.py
@@ -10,20 +10,26 @@ fin.close()
 # open output
 fout = open('./output.txt', 'w')
 
-# interpret lines
-for line in lines:
-    words = line.split()
+index = 0
+while index < len(lines):
+    words = lines[index].split()
     command = words[0]
     # comment
     if command == '#':
+        index += 1
         continue
     # print
     elif command == 'print':
         fout.write(' '.join(words[1:]))
+    # skip next line
+    elif command == 'skip':
+        index += 2
+        continue
     # unrecognized command
     else:
         fout.write('Command ' + command + ' not recognized')
     fout.write('\n')
+    index += 1
 
 # close output
 fout.close()
