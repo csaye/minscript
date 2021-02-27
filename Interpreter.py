d46b9dc4c4a7f7424e5d44ffcb1c4e36157f7cc6
Cooper Saye <coopersaye@gmail.com>
2021-02-27 16:24:09 -0700

    Implement print command.

--- Interpreter.py
+++ Interpreter.py
@@ -1,11 +1,26 @@
 # open input
-fin = open('./input.txt')
+fin = open('./input.txt', 'r')
+
+# read lines
+lines = fin.read().splitlines()
 
 # close input
 fin.close()
 
 # open output
-fout = open('./output.txt')
+fout = open('./output.txt', 'w')
+
+commands = ['print']
+
+# interpret lines
+for line in lines:
+    words = line.split()
+    command = words[0]
+    if command not in commands:
+        fout.write('Command ' + command + ' not recognized')
+    elif command == 'print':
+        fout.write(' '.join(words[1:]))
+    fout.write('\n')
 
 # close output
 fout.close()
